6df74c1c4fcf41d7046b8821adae698593b6f5cf
Antonov333 <antonovsergei333@yahoo.com>
2023-12-02 12:42:37 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    /**
+     *
+     * Tutorial application for Course Lesson1 Homework
+     *
+     * @param args not is use with current project
+     */
+    public static void main(String[] args) {
+
+    }
+}
